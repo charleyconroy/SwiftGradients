2da6d91c1b8f85a1d5fb5db519da3e3db1c2d988
Charles Conroy <conroycd@bc.edu>
2023-01-29 18:20:10 -0500

    Initial Commit

---
+++ Gradients/GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientsApp.swift
+//  Gradients
+//
+//  Created by Charley Conroy on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
